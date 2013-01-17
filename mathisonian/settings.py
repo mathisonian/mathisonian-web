14a82c246c3ee50a5e5c963fc558f584322472da
mathisonian <mc@mathisonian.com>
2013-01-17 03:03:19 -0500

    aws bucket

--- mathisonian/settings.py
+++ mathisonian/settings.py
@@ -270,8 +270,8 @@ AUTHENTICATION_BACKENDS = (
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 's3.mathisonian.com'
-STATIC_URL = 'http://' + 's3.mathisonian.com/'
+AWS_STORAGE_BUCKET_NAME = 'mathisonian-web'
+STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + 's3.amazonaws.com/'
 MEDIA_URL = STATIC_URL
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 AWS_QUERYSTRING_AUTH = False
